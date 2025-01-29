12167516842f8b6e4e3dbb34038a03f2254e5f6c
Coinnich <>
2025-01-29 17:47:14 +1300

    removed /index.html from assetsInclude

--- vite.config.ts
+++ vite.config.ts
@@ -23,5 +23,5 @@ export default defineConfig({
     target: 'es2023', // Set the target ES version for esbuild
     drop: ['console', 'debugger'], // Drop console.log and debugger statements
   }, // Set the mode to production
-  assetsInclude: ['data.ts', '/public/random.webp', '/index.html'], // Include data.ts in the build output
+  assetsInclude: ['data.ts', '/public/random.webp'], // Include data.ts in the build output
 })
