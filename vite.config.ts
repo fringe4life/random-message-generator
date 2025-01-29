458ee968dc30a5f08d8e0df4bc4ec821e1da9761
Coinnich <>
2025-01-29 17:32:58 +1300

    changed css build process to lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,30 @@
 import { defineConfig } from 'vite'
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-
-
+import { browserslistToTargets } from 'lightningcss'
+import browserslist from 'browserslist';
 // https://vite.dev/config/
 export default defineConfig({
+   css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%'))
+    }
+  },
   plugins: [svelte({
     preprocess: vitePreprocess({ script: true }), // Enable TypeScript preprocessing
     exclude: ['node_modules/**', '/src/styles.css', '/src/reset.css'] // Exclude node_modules from preprocessing
   })],
   build: {
+    cssMinify: 'lightningcss', // Use LightningCSS for CSS minification
     target: 'es2023', // Set the target ES version
     minify: true, // Enable minification
     rollupOptions: {
       input: ['/index.html','src/main.ts'], // Specify the entry point
       output:{
         inlineDynamicImports: false, // Disable inlining dynamic imports
-      }
+        
+      },
+      external: ['svelte']
     },
     
   },
