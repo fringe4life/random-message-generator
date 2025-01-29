06f44cc23ca75a7717b0bf6b5871aa900a205357
Coinnich <>
2025-01-29 14:13:07 +1300

    added node types and changed rollupOptions to include all svelte files

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import fs from 'node:fs'
+import path from 'node:path'
 
+// Function to get all Svelte files in a directory
+function getSvelteFiles(dir: string): string[] {
+  const files = fs.readdirSync(dir)
+  const svelteFiles = files.filter(file => file.endsWith('.svelte'))
+  return svelteFiles.map(file => path.join(dir, file))
+}
+
+// Get all Svelte files in the src directory
+const svelteFiles = getSvelteFiles(path.resolve(__dirname, 'src'))
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte({
@@ -10,7 +21,7 @@ export default defineConfig({
     target: 'es2023', // Set the target ES version
     minify: true, // Enable minification
     rollupOptions: {
-      input: '/index.html',
+      input: [...svelteFiles, '/index.html'], // Include all Svelte files and the index.html file
       output: {
         inlineDynamicImports: true,
       },
