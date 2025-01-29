2ebbf128aa2079a1105b15e03eb3e260359baca6
Coinnich <>
2025-01-30 10:54:13 +1300

    updated app to concatenate the message together from objects

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,50 @@
 <script lang="ts">
-  import codingMessages from "./data";
+  import {startOfMessage, endOfMessage} from "./data";
   import type { Message } from "./data";
   import MessageCard from "./lib/MessageCard.svelte";
 
   // need to use state to handle random coding messages
   const randomCodingMessages = $state.raw(getRandomCodingMessages());
 
+  $inspect(randomCodingMessages);
 
-  
   // need to pass each specific coding message to the card component
+  function getRandomIndexes(firstArray: Message[], secondArray: Message[]): number[] {
+    const randomIndex1 = Math.floor(Math.random() * firstArray.length);
+    const randomIndex2 = Math.floor(Math.random() * secondArray.length);
+    return [randomIndex1, randomIndex2];
+  }
+
+  function createNewMessage(...indexes: number[]): Message {
+    return {
+					message: `${startOfMessage[indexes[0]].message} ${endOfMessage[indexes[1]].message}`,
+					id:	startOfMessage[indexes[0]].id
+				} satisfies Message;
+  }
+
+  function addIndexesUsed(indexes: number[], indexesAlreadyIncluded: Set<number>): void {
+    for (const i of indexes) {
+      indexesAlreadyIncluded.add(i);
+    }
+  }
 
   /**
    * @abstract Get random coding messages
    * @returns {string[]} Random coding messages
    */
   function getRandomCodingMessages(): Message[] {
-    const length = Math.trunc(codingMessages.length / 2);
+    const length = Math.trunc((startOfMessage.length  ) / 2);
     const randomMessages = [];
-    const indexesAlreadyIncluded = new Set();
+    const indexesAlreadyIncluded = new Set<number>();
     for (let i = 0; i < length; i++) {
-      const randomIndex = Math.floor(Math.random() * codingMessages.length);
-      if(indexesAlreadyIncluded.has(randomIndex)) {
+      const randomIndexes = getRandomIndexes(startOfMessage, endOfMessage);
+      if(indexesAlreadyIncluded.has(randomIndexes[0]) || indexesAlreadyIncluded.has(randomIndexes[1])) {
         i--;
         continue;
       }
-      randomMessages.push(codingMessages[randomIndex]);
-      indexesAlreadyIncluded.add(randomIndex);
+      
+      randomMessages.push(createNewMessage(...randomIndexes));
+      addIndexesUsed(randomIndexes, indexesAlreadyIncluded);
     }
     return randomMessages;
   }
